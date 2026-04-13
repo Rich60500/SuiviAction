d6e84593483fdfaddd6689954de8020d10d11c48
Rich60500 <richard.denis@me.com>
2026-04-13 12:33:51 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker — Suivi Actions SNCF
 // Base path GitHub Pages : /SuiviAction/
-const CACHE   = 'sncf-actions-v3';
+const CACHE   = 'sncf-actions-v4';
 const BASE    = '/SuiviAction';
 const ASSETS  = [
   `${BASE}/`,
